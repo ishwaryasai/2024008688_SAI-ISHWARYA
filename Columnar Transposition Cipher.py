1ac485b829b873f180d04b92f0a58391da523233
ishwaryasai <sraparth@student.gitam.edu>
2026-09-18 12:03:35 +0530

    Columnar_Transposition_Cipher.py

--- Columnar Transposition Cipher.py
+++ Columnar Transposition Cipher.py
@@ -1,15 +1,13 @@
-# Columnar Transposition Cipher
+//Columnar Transposition Cipher
 
 text = input("Enter text: ").replace(" ", "").upper()
 key = [3, 1, 4, 2]
-
-# Add X
 while len(text) % 4 != 0:
     text += "X"
 
-# Encryption
-encrypted = ""
+// Encryption
 
+encrypted = ""
 for n in range(1, 5):
     col = key.index(n)
 
@@ -18,15 +16,14 @@ for n in range(1, 5):
 
 print("Encrypted:", encrypted)
 
-# Decryption
+// Decryption
+
 decrypted = [""] * len(text)
 k = 0
-
 for n in range(1, 5):
     col = key.index(n)
 
     for i in range(col, len(text), 4):
         decrypted[i] = encrypted[k]
         k += 1
-
-print("Decrypted:", "".join(decrypted))
+print("Decrypted:", ""decrypted)
